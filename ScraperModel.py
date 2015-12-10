fc953127367f0750eeaf27d097211382ca5288b1
unknown <phil_hu.swim@hotmail.com>
2015-12-10 01:25:38 +0100

    there are currently issues iterating through saved links

--- ScraperModel.py
+++ ScraperModel.py
@@ -1,19 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
 import sqlite3
+from urllib import urlopen
 
 pages = 1 #HOW MANY PAGES OF PROJECTS DO YOU WANT TO SCRAPE? (20 PROJECTS PER PAGE)
 
 #setting up sqlite3
 conn = sqlite3.connect('kickstarter-project-links.db')  #creates sqlite database
 c = conn.cursor()   #starts creation of database
-sqlLinks = "SELECT * FROM ProjectLinks" # WHERE Link =?" #to read from database
+sqlLinks = "SELECT * FROM ProjectLinks;" # WHERE Link =?" #to read from database
 
 #function for creating a table
 def tableCreate():
     #ProjectLinks for scraped links && ProjectData for scraped project-specific data
     c.execute("CREATE TABLE ProjectLinks(Link TEXT)")
-    c.execute("CREATE TABLE ProjectData(ProjectTitle TEXT, TotalBackers INT, TotalPledged INT, Goal INT, TimeLeft TEXT, EndDate TEXT, TotalUpdates INT, TotalComments INT, ImageCount INT, Description TEXT)")
+    c.execute("CREATE TABLE ProjectData(ProjectTitle TEXT, TotalBackers INT, TotalPledged INT, Goal INT, ProjectLength INT, EndDate TEXT, HoursLeft REAL, TotalUpdates INT, TotalComments INT, Description TEXT)")
 
 ##################################################LINK SCRAPING#####################################################
 
@@ -57,13 +58,13 @@ commentsNum = 0
 descriptionTxt = ""
 
 #function for adding data to ProjectData.db
-def dataEntryData(projectTitle, backersNum, totalPledged, goal, timeLeft, endDate, updatesNum, commentsNum, imgCount, descriptionTxt):
-    c.execute("INSERT INTO ProjectData(ProjectTitle, TotalBackers, TotalPledged, Goal, ProjectLength, EndDate, HoursLeft, TotalUpdates, TotalComments, Description) VALUES (?,?,?,?,?,?,?,?,?,?)",
+def dataEntryData(projectTitle, backersNum, totalPledged, goal, projLength, endDate, hrsLeft, updatesNum, commentsNum, descriptionTxt):
+    c.execute("INSERT INTO `ProjectData`(ProjectTitle, TotalBackers, TotalPledged, Goal, ProjectLength, EndDate, HoursLeft, TotalUpdates, TotalComments, Description) VALUES (?,?,?,?,?,?,?,?,?,?)",
               (projectTitle, backersNum, totalPledged, goal, projLength, endDate, hrsLeft, updatesNum, commentsNum, descriptionTxt,))
     conn.commit() #absolutely need to do this to save data to table
             
 #Collecting all of the data from the individual projects
-def CollectData():
+def collectData():
     for row in c.execute(sqlLinks):
         projectUrl = str(row).replace("(u'","").replace("',)", "")
         r = requests.get(projectUrl)
@@ -73,27 +74,30 @@ def CollectData():
         raw_data = soup.find_all("section")
 
         for data in raw_data:
-            try: print data.find("h2", {"class":"normal mb1"}).text #title
+            try: projectTitle = data.find("h2", {"class":"normal mb1"}).text #title
             except: pass
-            try: print data.find("data", {"itemprop":"Project[backers_count]"}).text #total backers
+            try: backersNum = data.find("data", {"itemprop":"Project[backers_count]"}).text #total backers
             except: pass
-            try: print data.find("data", {"itemprop":"Project[pledged]"}).text #funding
+            try: totalPledged = data.find("data", {"itemprop":"Project[pledged]"}).text #funding
             except: pass
-            try: print data.find("span", {"class":"money usd no-code"}).text #goal
+            try: goal = data.find("span", {"class":"money usd no-code"}).text #goal
             except: pass
             try:
                 for span_data in data.find_all('span', id=True):
-                    print span_data['data-duration'] #project length
-                    print span_data['data-end_time'] #project end date
-                    print span_data['data-hours-remaining'] #hours left for funding
+                    projLength = span_data['data-duration'] #project length
+                    endDate = span_data['data-end_time'] #project end date
+                    hrsLeft = span_data['data-hours-remaining'] #hours left for funding
             except: pass
-            try: print data.find("a", {"data-content":"updates"}).text.replace("Updates\n(","").replace(")\n","") #number of updates
+            try: updatesNum = data.find("a", {"data-content":"updates"}).text.replace("Updates\n(","").replace(")\n","") #number of updates
             except: pass
             try:
                 for data in data.find_all('span', id=True):
-                    print data['data-comments-count'] #number of comments 
+                    commentsNum = data['data-comments-count'] #number of comments 
             except: pass
-            try: print data.find("div", {"class":"full-description"}).text.replace("\n","") #description text
+            try: descriptionTxt = data.find("div", {"class":"full-description"}).text.replace("\n","") #description text
+            except: pass
+            try:
+                dataEntryData(projectTitle, backersNum, totalPledged, goal, projLength, endDate, hrsLeft, updatesNum, commentsNum, description)
             except: pass
         print str(row) + " Complete"
     print "DONE"
